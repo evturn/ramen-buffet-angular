6edf3ec920bd64e67ba36455aabaf9c620344654
Evan Turner <evturn@gmail.com>
2015-02-23 03:11:29 -0500

    main controller fix

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,27 +1,27 @@
 angular.module('todoController', [])
-	.controller('mainController', function($scope, $http) {
-		$scope.formData = {};
+
+.controller('mainController', function($scope, $http, Todos) {
+  $scope.formData = {};
 
   Todos.get()
-    .success(function(data) {
-      $scope.todos = data;
-    });
+      .success(function(data) {
+        $scope.todos = data;
+      });
 
   $scope.createTodo = function() {
-    if (!$.isEmptyObjects($scope.formData)) {
+    if (!$.isEmptyObject($scope.formData)) {
       Todos.create($scope.formData)
-      .success(function(data) {
-        $scope.formData = {};
-        $score.todos = data;
-      });
+        .success(function(data) {
+          $scope.formData = {};
+          $scope.todos = data;
+        });
     }
   };
 
-  $score.deleteTodo = function(id) {
+  $scope.deleteTodo = function(id) {
     Todos.delete(id)
       .success(function(data) {
         $scope.todos = data;
       });
   };
-
 });
