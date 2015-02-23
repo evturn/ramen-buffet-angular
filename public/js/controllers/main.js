abef60a920a676fab2683848d9e7b2c93596c258
Evan Turner <evturn@gmail.com>
2015-02-23 03:06:36 -0500

    moved  code outside of controller and into service

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,33 +2,26 @@ angular.module('todoController', [])
 	.controller('mainController', function($scope, $http) {
 		$scope.formData = {};
 
-		$http.get('/api/todos')
-			.success(function(data) {
-				$scope.todos = data;
-			})
-			.error(function(data) {
-				console.log('Error: ' + data);
-			});
+  Todos.get()
+    .success(function(data) {
+      $scope.todos = data;
+    });
 
-    $scope.createTodo = function() {
-      $http.post('/api/todos', $scope.formData)
-        .success(function(data) {
-          $scope.formData = {};
-          $scope.todos = data;
-        })
-        .error(function(data) {
-          console.log('Error: ' + data);
-        });
-    };
-
-    $scope.deleteTodo = function(id) {
-	    $http.delete('/api/todos/' + id)
-        .success(function(data) {
-          $scope.todos = data;
-      })
-        .error(function(data) {
-          console.log('Error: ' + data);
+  $scope.createTodo = function() {
+    if (!$.isEmptyObjects($scope.formData)) {
+      Todos.create($scope.formData)
+      .success(function(data) {
+        $scope.formData = {};
+        $score.todos = data;
       });
-		};
+    }
+  };
 
-	});
+  $score.deleteTodo = function(id) {
+    Todos.delete(id)
+      .success(function(data) {
+        $scope.todos = data;
+      });
+  };
+
+});
