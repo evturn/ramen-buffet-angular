e01fca0ec7d5c93df43bed8f8eebf7f19bf3fa84
Evan Turner <evturn@gmail.com>
2015-02-23 02:19:03 -0500

    removed routes from server and created module

--- server.js
+++ server.js
@@ -16,60 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
-var Todo = mongoose.model('Todo', {
-	text : String,
-	done : Boolean
-});
-
-app.get('/api/todos', function(req, res) {
-
-  Todo.find(function(err, todos) {  
-    if (err)
-      res.send(err)
-
-    res.json(todos);
-  });
-});
-   
-app.post('/api/todos', function(req, res) {
-
-  Todo.create({
-    text : req.body.text,
-    done : false
-  }, function(err, todo) {
-      if (err)
-        res.send(err);
-
-		Todo.find(function(err, todos) {
-		    if (err)
-		        res.send(err)
-		    res.json(todos);
-		});
-  
-  });
-
-});
- 
-app.delete('/api/todos/:todo_id', function(req, res) {
-  Todo.remove({
-    _id : req.params.todo_id
-  }, function(err, todo) {
-      if (err)
-        res.send(err);
-
-      Todo.find(function(err, todos) {
-        if (err)
-          res.send(err)
-          res.json(todos);
-      });
-  });
-});
-
-
-app.get('*', function(req, res) {
-  res.sendfile('./public/index.html');
-});
-
+require('./app/routes')(app);
 
 app.listen(port);
 console.log("App listening on port " + port);
