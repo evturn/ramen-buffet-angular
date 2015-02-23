7410e3a0fa1669fc67c45c97e08a1eb6d430d47e
Evan Turner <evturn@gmail.com>
2015-02-23 06:45:12 -0500

    connecting all accounts

--- app/routes.js
+++ app/routes.js
@@ -52,6 +52,35 @@ app.post('/login', passport.authenticate('local-login', {
       failureRedirect : '/'
     }));
 
+  
+  app.get('/connect/local', function(req, res) {
+    res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+  });
+
+  app.post('/connect/local', passport.authenticate('local-signup', {
+    successRedirect : '/profile',
+    failureRedirect : '/connect/local',
+    failureFlash : true
+  }));
+
+  app.get('/connect/facebook', passport.authorize('facebook', { scope : 'email' }));
+
+  app.get('/connect/facebook/callback',
+    passport.authorize('facebook', {
+      successRedirect : '/profile',
+      failureRedirect : '/'
+  }));
+
+  app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+
+  app.get('/connect/twitter/callback',
+    passport.authorize('twitter', {
+      successRedirect : '/profile',
+      failureRedirect : '/'
+  }));
+
+
+
 };
 
 function isLoggedIn(req, res, next) {   
