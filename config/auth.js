70ee536fb133d55632b9a9972c22ac4ecfdd6cc5
Evan Turner <evturn@gmail.com>
2015-02-23 03:43:16 -0500

    twitter OAuth created

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  'twitterAuth' : {
+    'consumerKey'       : 'process.env.TWITTER_KEY',
+    'consumerSecret'    : 'process.env.TWITTER_SECRET',
+    'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+  }
+
+};
