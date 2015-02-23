90f5c151325113fc0eff0a8ac17f509aa74a8800
Evan Turner <evturn@gmail.com>
2015-02-23 06:51:14 -0500

    passport for linking facebook to signed in user created

--- config/passport.js
+++ config/passport.js
@@ -63,32 +63,45 @@ module.exports = function(passport) {
   passport.use(new FacebookStrategy({
     clientID        : configAuth.facebookAuth.clientID,
     clientSecret    : configAuth.facebookAuth.clientSecret,
-    callbackURL     : configAuth.facebookAuth.callbackURL
-
+    callbackURL     : configAuth.facebookAuth.callbackURL,
+    passReqToCallback : true
   }, 
-  function(token, refreshToken, profile, done) {    
+  function(req, token, refreshToken, profile, done) {    
     process.nextTick(function() {
-      User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
-        if (err)
-          return done(err);
-        if (user) {
-            return done(null, user);
-        } else {
-        var newUser            = new User();
-        newUser.facebook.id    = profile.id;
-        newUser.facebook.token = token;
-        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
-        newUser.facebook.email = profile.emails[0].value;
-        newUser.save(function(err) {
+      if (!req.user) {
+        User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+          if (err)
+            return done(err);
+          if (user) {
+              return done(null, user);
+          } else {
+          var newUser            = new User();
+          newUser.facebook.id    = profile.id;
+          newUser.facebook.token = token;
+          newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+          newUser.facebook.email = profile.emails[0].value;
+          newUser.save(function(err) {
+            if (err)
+              throw err;
+            return done(null, newUser);
+          });
+          }
+        });
+      } else {
+        var user            = req.user;
+        user.facebook.id    = profile.id;
+        user.facebook.token = token;
+        user.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName;
+        user.facebook.email = profile.emails[0].value;
+        user.save(function(err) {
           if (err)
             throw err;
-          return done(null, newUser);
+          return done(null, user);
         });
-        }
-      });
+      }
     });
   }));
-  
+
   passport.use(new TwitterStrategy({
     consumerKey     : configAuth.twitterAuth.consumerKey,
     consumerSecret  : configAuth.twitterAuth.consumerSecret,
